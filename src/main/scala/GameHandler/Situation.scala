be5c006ebca3d7cca7cffafe2a558be2b49eeabb
matbesancon <mathieu.besancon@gmail.com>
2016-06-10 17:37:25 +0200

    Situation toString method

--- src/main/scala/GameHandler/Situation.scala
+++ src/main/scala/GameHandler/Situation.scala
@@ -10,12 +10,15 @@ import Misc._;
 
 // situation: Country -> (troops present, authorized to move, player)
 class CountrySituation(val troops: DefenseTroops,val player: Player){
-  override def toString: String = "troops "+ troops.number + ", player: "+player.name
+  override def toString = "player: " + player.name+ ", troops " + troops.number
 }
 
 class Situation(val troopMap: Map[Country,CountrySituation]){
-  def updated(country: Country,countrySituation: CountrySituation): Situation = {
-    val newTroop = troopMap.updated(country,countrySituation)
-    new Situation(newTroop)
-  }
+  def updated(country: Country,countrySituation: CountrySituation): Situation = new Situation(
+    troopMap.updated(country,countrySituation)
+  )
+  override def toString = "Current situation: " + troopMap.map(
+    (pair:(Country,CountrySituation))=> "\n Country "+pair._1.name + ", "+ pair._2.toString
+  ).toString
+
 }
